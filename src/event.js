5c1b24156250cc1d9ccafebac8c56997996dc90c
Emilio Silva Schlenker <e18r@disroot.org>
2020-09-17 10:32:17 -0500

    put event info in its own component

--- src/event.js
+++ src/event.js
@@ -6,7 +6,6 @@ import React, {
   useState,
 } from 'react';
 import { useParams } from 'react-router-dom';
-import Markdown from 'react-markdown';
 import abi from '../contracts/ProxyEvent.abi.json';
 import { Web3Context, AccountContext, BackendContext } from './coinosis';
 import { Link, Loading, NoContract, convertDates, useGetUser } from './helpers';
@@ -19,6 +18,7 @@ import Result from './result';
 import Footer from './footer';
 import { differenceInDays, formatDistance } from 'date-fns'
 import { useT, useLocale, } from './i18n';
+import EventInfo from './eventInfo';
 
 export const eventStates = {
   EVENT_CREATED: 0,
@@ -327,7 +327,7 @@ const Event = () => {
         >
           <Account large={true} />
         </div>
-        <Info event={event} />
+        <EventInfo event={event} />
       </div>
     );
   }
@@ -342,7 +342,7 @@ const Event = () => {
           end={event.end}
           eventState={eventState}
         />
-        <Info event={event} />
+        <EventInfo event={event} />
         <NoContract currency={event.currency} />
       </div>
     );
@@ -409,7 +409,7 @@ const Event = () => {
         />
       ) }
       { !attending && (
-        <Info
+        <EventInfo
           event={event}
           userState={userState}
           eventState={eventState}
@@ -488,77 +488,6 @@ const Event = () => {
         currency={event.currency}
       />
     </ContractContext.Provider>
-  );
-}
-
-const Info = ({ event, userState, eventState }) => {
-  const t = useT();
-  return (
-    <div>
-      { event.broadcastID && (
-      <div
-        css={`
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-          `}
-      >
-        <div
-          css={`
-              margin-top: 50px;
-              display: flex;
-              justify-content: center;
-            `}
-        >
-          { userState === userStates.UNREGISTERED
-            && eventState < eventStates.EVENT_ABOUT_TO_END
-            && t('dont_want_to_participate') }
-          <a
-            css={`
-                margin: 0 5px;
-                color: black;
-                &:visited {
-                  color: black;
-                }
-              `}
-            href={`https://youtu.be/${ event.broadcastID }`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            { t('watch_on_youtube') }
-          </a>
-          { userState === userStates.UNREGISTERED
-            && eventState < eventStates.EVENT_ABOUT_TO_END
-            && t('at_no_cost') }
-        </div>
-        <iframe
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${ event.broadcastID }`}
-          frameBorder="0"
-          allow={'accelerometer; autoplay; encrypted-media; gyroscope; '
-                 + 'picture-in-picture'}
-          allowFullScreen
-        >
-        </iframe>
-      </div>
-      ) }
-      <div
-        css={`
-          margin: 20px;
-          background: #f8f8f8;
-          padding: 10px;
-          border-radius: 4px;
-          border: 1px solid #e8e8e8;
-          box-shadow: 1px 1px #e8e8e8;
-        `}
-      >
-        <Markdown
-          source={ event.description }
-          linkTarget="_blank"
-        />
-      </div>
-    </div>
   );
 }
 
